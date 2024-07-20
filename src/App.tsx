d5fd64a376371bf5e886817c4ea8580fc9bdb804
Chandra Kumar Rajwal <122195878+imckr@users.noreply.github.com>
2024-07-20 10:37:48 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Blogs from "./pages/Blogs";
 import About from "./pages/About";
 import BlogPage from "./pages/BlogPage";
 import "./App.css";
-// import PreLoader from "./components/PreLoader/PreLoader";
+import PreLoader from "./components/PreLoader/PreLoader";
 
 const App: React.FC = () => {
-  
-  // const [active, setActive] = useState<boolean>(true);
+  const [active, setActive] = useState<boolean>(true);
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setActive(false);
-  //   }, 5000);
-  // });
+  useEffect(() => {
+    setTimeout(() => {
+      setActive(false);
+    }, 5000);
+  });
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" Component={Blogs} />
-          <Route path="/about" Component={About} />
-          <Route path="/blogPage" Component={BlogPage} />
-        </Routes>
-      </Router>
+      {active ? (
+        <PreLoader />
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/" Component={Blogs} />
+            <Route path="/about" Component={About} />
+            <Route path="/blogPage" Component={BlogPage} />
+          </Routes>
+        </Router>
+      )}
     </>
   );
 };
